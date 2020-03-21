46ef442b945bbcadd5eb2f1c02124f09f95a914b
Max <60153365+Vier1996@users.noreply.github.com>
2020-03-21 20:31:16 +0300

    adddas

---
+++ Project/Project_Y/Assets/Scenes/DD.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DD : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("aaaaaaaaaaaaaaaaaaaaaa");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
